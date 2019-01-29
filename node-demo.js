c2b274803515baf1bca19114e38d76e22bcb9e8b
Adrian Todorov <atodorov@cloudops.com>
2019-01-28 22:00:53 -0500

    Hello Montreal

--- node-demo.js
+++ node-demo.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3000;
 
 const handler = (req, res) => {
     console.log('Server received request.');
-    figlet('Hello Montreal 2019 !!!', (err, data) => {
+    figlet('Hello Montreal!!!', (err, data) => {
       if (err) {
             console.log('Something went wrong... ');
             console.dir(err);
